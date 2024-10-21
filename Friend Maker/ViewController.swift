c73a97127dd464dde05ca6a965e5caede24b9846
nuaman <nuaman.s@axeltechnologies.com>
2024-10-21 17:22:36 +0530

    Initial Commit

---
+++ Friend Maker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Friend Maker
+//
+//  Created by Muhammed Nuaman Sadiq on 21/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
